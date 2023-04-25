12da6ef0ddc833c34d995be823e246e9a031a320
etan-nestor <etannestor45@gmail.com>
2023-04-25 15:08:51 +0000

    6th commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'student-home',
+    redirectTo: 'options-pay',
     pathMatch: 'full'
   },
   {
