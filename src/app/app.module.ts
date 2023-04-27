02f9e798b2ec3ccb0660481d31936ca92ee9db9c
etan-nestor <etannestor45@gmail.com>
2023-04-27 18:13:36 +0000

    nestor-test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-// import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 // import { environment } from '../environments/environment';
 // import { AngularFireModule } from '@angular/fire/compat';
 // import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+// import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/new-student-sign-up/new-student-sign-up.page.ts
+++ src/app/new-student-sign-up/new-student-sign-up.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-// import { AngularFireAuth } from '@angular/fire/compat/auth';
-// import { AngularFirestore } from '@angular/fire/compat/firestore';
+// import { AngularFireAuth} from '@angular/fire/compat/auth';
+// import { AngularFirestore  } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-new-student-sign-up',
@@ -9,22 +9,15 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./new-student-sign-up.page.scss'],
 })
 export class NewStudentSignUpPage implements OnInit {
-  // annee_bac!: string;
-  // email!: string;
-  // id!: string;
-  // nom!: string;
-  // password!: string;
-  // prenom!: string;
-  // profil!: string;
-  // serie_bac!: string;
-  // username!: string;
+    // username = '';
+    // email = '';
+    // password = '';
 
   constructor(
     public nc: NavController,
-    // private afAuth: AngularFireAuth,
-    // private firestore: AngularFirestore
-  ) { }
-
+    // private auth: AngularFireAuth,
+    // private firestore: AngularFirestore 
+  ) {}
   ngOnInit() {
   }
 
@@ -32,36 +25,28 @@ export class NewStudentSignUpPage implements OnInit {
     this.nc.navigateForward('/login');
   }
 
-//   async sign_up() {
-//     try {
-//     // Créer un nouvel utilisateur avec l'e-mail et le mot de passe
-//     const userCredential = await this.afAuth.createUserWithEmailAndPassword(
-//       this.email,
-//       this.password
-//     );
-//     const user = userCredential.user;
+  async signUp() {
+      // try {
+      //   const userCredential = await this.auth.createUserWithEmailAndPassword(this.email, this.password);
 
-//     if (user) { // Vérifier si user n'est pas null
-//       // Ajouter les données supplémentaires de l'étudiant à la collection Firebase
-//       await this.firestore.collection('N-etudiant').doc(user.uid).set({
-//         annee_bac: this.annee_bac,
-//         email: this.email,
-//         id: user.uid,
-//         nom: this.nom,
-//         prenom: this.prenom,
-//         profil: this.profil,
-//         serie_bac: this.serie_bac,
-//         username: this.username
-//       });
+      //   if (userCredential && userCredential.user) {
+      //     await userCredential.user.updateProfile({
+      //       displayName: this.username
+      //     });
 
-//       // Rediriger l'utilisateur vers la page de connexion
-//       this.nc.navigateForward('/login');
-//     } else {
-//       console.log('user est null');
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+      //     await this.firestore.collection('N-etudiant').doc(userCredential.user.uid).set({
+      //       email: this.email,
+      //       username: this.username,
+      //       uid: userCredential.user.uid
+      //     });
+
+      //     // Navigate to the login page
+      //     this.nc.navigateForward('/login');
+      //   }
+
+      // } catch (error) {
+      //   console.log('Error creating user:', error);
+      // }
+    }
 
 }
