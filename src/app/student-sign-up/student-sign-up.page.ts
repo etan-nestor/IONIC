55875d3ae9c195d395a02de54cb83b08608daec2
etan-nestor <etannestor45@gmail.com>
2023-04-27 11:20:11 +0000

    again

--- src/app/student-sign-up/student-sign-up.page.ts
+++ src/app/student-sign-up/student-sign-up.page.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import {Router} from '@angular/router';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-// import { getDatabase, ref, set } from "firebase/database";
-import { getFirestore,doc,setDoc,addDoc,collection} from "firebase/firestore";
 
 
 @Component({
@@ -13,13 +9,9 @@ import { getFirestore,doc,setDoc,addDoc,collection} from "firebase/firestore";
 })
 export class StudentSignUpPage implements OnInit {
 
-  id=0;
-  INE:any;
-  username:any;
-  email:any;
-  pass:any;
-
-  constructor(public nc:NavController,private router:Router, private auth:AngularFireAuth) { }
+  constructor(public nc: NavController,
+    // private router: Router, private auth: AngularFireAuth
+    ) { }
 
   ngOnInit() {
   }
@@ -28,51 +20,7 @@ export class StudentSignUpPage implements OnInit {
   }
   sign_up() {
     // this.nc.navigateForward('/student-home');
-
-
-    this.INE=((document.getElementById("INE") as HTMLInputElement).value);
-    this.username=((document.getElementById("username") as HTMLInputElement).value);
-    this.email=((document.getElementById("email") as HTMLInputElement).value);
-    this.pass=((document.getElementById("password") as HTMLInputElement).value);
-    this.id+=1;
-
-
-   try{
-    const db = getFirestore();
-    addDoc(collection(db,"users" ),{
-      INE:this.INE,
-      username: this.username,
-      email: this.email,
-      password:this.pass
-    })
-
-   }catch(e){
-    window.alert(e)
-     this.router.navigateByUrl('signup')
-   }
-
-    this.auth.
-    createUserWithEmailAndPassword(this.email, this.pass)
-      .then((userCredential) => {
-
-        if (userCredential.user){
-
-          window.alert("logged in ")
-          this.router.navigateByUrl('/login')
-        }
-      })
-      .catch((error) => {
-        const errorCode = error.code
-        const errorMessage = error.message
-        window.alert(errorMessage)
-        // ..
-      });
-
-
-
-
-
-}
+  }
 
 
 }
